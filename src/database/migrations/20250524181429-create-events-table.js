c2d98c3ad995390e039ff67977cbc0a7ab62807c
Baunas13 <k.a.u.a.n@outlook.com>
2025-05-24 15:25:50 -0300

    Adiçao das colunas da tabela events

--- src/database/migrations/20250524181429-create-events-table.js
+++ src/database/migrations/20250524181429-create-events-table.js
@@ -2,21 +2,70 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+
+    await queryInterface.createTable('events', {
+
+      id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4
+      },
+
+      nomeEvento: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+
+      descricao: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+
+      data: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+
+      local: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+
+      nicho: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+
+      precificacao: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+
+      limiteInscricoes: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+
+
+    });
+
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down(queryInterface, Sequelize) {
+
+    await queryInterface.dropTable('events');
+
   }
 };
